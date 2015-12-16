e874f3b6ffa90d9b5eeb0ccc52785edf3d1d1438
Adam-Greenfield <ajamesgreenfield@gmail.com>
2015-12-16 18:27:30 +0000

    updated config.js (gerry)

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'secret': 'process.env.CHAMPION_APP_SECRET'
-  'database': 'process.env.MONGOLAB_URI' || 'mongodb://localhost:27017/championAPI'
+  'secret': process.env.CHAMPION_APP_SECRET,
+  'database': process.env.MONGOLAB_URI || 'mongodb://localhost:27017/championAPI'
 };
